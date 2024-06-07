9f4d71bd5f0b74deab688986b121f5a1db57e901
Alberto Alegre - Enti <alberto.alegre@enti.cat>
2024-06-07 17:09:31 +0200

    Update

--- Assets/Scripts/TriggerEvent.cs
+++ Assets/Scripts/TriggerEvent.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,54 +6,58 @@ using UnityEngine.Events;
 public class TriggerEvent : MonoBehaviour
 {
     public string[] tags;
+    public LayerMask mask;
     public UnityEvent OnColliderEnter;
     public UnityEvent OnColliderExit;
     public UnityEvent OnColliderStay;
     public UnityEvent OnTriggerEnter;
     public UnityEvent OnTriggerExit;
-    public UnityEvent OnTrggerStay;
+    public UnityEvent OnTriggerStay;
 
-    
+    public bool CanEnter(GameObject go)
+    {
+        return tags.Contains(go.tag) && (mask & (1 << go.layer)) != 0;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (tags.Contains(collision.gameObject.tag))
+        if (CanEnter(collision.gameObject))
         {
             OnColliderEnter.Invoke();
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (tags.Contains(collision.gameObject.tag))
+        if (CanEnter(collision.gameObject))
         {
             OnColliderExit.Invoke();
         }
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (tags.Contains(collision.gameObject.tag))
+        if (CanEnter(collision.gameObject))
         {
             OnColliderStay.Invoke();
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (tags.Contains(collision.gameObject.tag))
+        if (CanEnter(collision.gameObject))
         {
-            OnColliderEnter.Invoke();
+            OnTriggerEnter.Invoke();
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (tags.Contains(collision.gameObject.tag))
+        if (CanEnter(collision.gameObject))
         {
-            OnColliderExit.Invoke();
+            OnTriggerExit.Invoke();
         }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (tags.Contains(collision.gameObject.tag))
+        if (CanEnter(collision.gameObject))
         {
-            OnColliderStay.Invoke();
+            OnTriggerStay.Invoke();
         }
     }
 }
